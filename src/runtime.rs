fed5328a758a9799e88bdab829157f21c644992a
Mikey O'Brien <hmobrienv@gmail.com>
2026-03-02 00:23:56 -0600

    fix: ensure complete runtime.rs

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,1 +1,33 @@
-RUNTIME_CONTENT_PLACEHOLDER
+use crate::ir::{IrCallTarget, IrForGenerator, IrOp, IrPattern, IrProgram};
+use crate::native_runtime;
+use std::collections::HashMap;
+use std::fmt;
+
+const ENTRYPOINT: &str = "Demo.run";
+const FOR_REDUCE_ACC_BINDING: &str = "__tonic_for_acc";
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct RuntimeClosure {
+    params: Vec<String>,
+    ops: Vec<IrOp>,
+    env: HashMap<String, RuntimeValue>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum RuntimeValue {
+    Int(i64),
+    Float(String),
+    Bool(bool),
+    Nil,
+    String(String),
+    Atom(String),
+    ResultOk(Box<RuntimeValue>),
+    ResultErr(Box<RuntimeValue>),
+    Tuple(Box<RuntimeValue>, Box<RuntimeValue>),
+    Map(Vec<(RuntimeValue, RuntimeValue)>),
+    Keyword(Vec<(RuntimeValue, RuntimeValue)>),
+    List(Vec<RuntimeValue>),
+    Range(i64, i64),
+    SteppedRange(i64, i64, i64),
+    Closure(Box<RuntimeClosure>),
+}
